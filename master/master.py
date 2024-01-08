c2159e2c2a02e1b151263dfba4c674c3b6656a03
nikitoz37 <navigator1399@gmail.com>
2024-01-08 11:51:50 +0300

    16

--- master/master.py
+++ master/master.py
@@ -5,8 +5,24 @@ from flask import Flask
 from flask import request, render_template, redirect, url_for, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 #from config import BaseConfig
-from os import environ
+#from os import environ
+import os
 
+
+
+with open(os.environ['POSTGRES_USER_FILE']) as f:
+    _db_user = f.read()
+
+with open(os.environ['POSTGRES_PASSWORD_FILE']) as f:
+    _db_pass = f.read()
+
+class BaseConfig(object):
+    DEBUG = os.environ['DEBUG']
+    DB_NAME = os.environ['POSTGRES_DB']
+    DB_USER = _db_user
+    DB_PASS = _db_pass
+    DB_PORT = os.environ['DATABASE_PORT']
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASS}@postgres:{DB_PORT}/{DB_NAME}'
 
 
 app = Flask(__name__)
