e1e2766bd7ecbf398d46821b32b0bcb6142680a9
nikitoz37 <navigator1399@gmail.com>
2024-01-08 11:17:14 +0300

    15

--- master/master.py
+++ master/master.py
@@ -41,8 +41,7 @@ class User(db.Model):
         return {'id': self.id,'username': self.username, 'email': self.email}
 
 
-#with app.app_context():
-db.create_all()
+
 
 
 #create a test route
@@ -90,6 +89,8 @@ def index():
     return 'Im master'
 
 
+with app.app_context():
+    db.create_all()
 
 
 
